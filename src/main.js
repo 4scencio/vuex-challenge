0599ec7d66d793e664fec4c327f4de81026073cf
4scencio <alessandroascencio@protonmail.com>
2021-02-14 10:52:54 -0800

    import global dependencies

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
+import '../src/assets/css/main.css'
+import 'animate.css'
 
 createApp(App).use(store).mount('#app')
