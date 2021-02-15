1d6c472ad44f3b46162bc4552570a67dcab2f41b
4scencio <alessandroascencio@protonmail.com>
2021-02-15 13:41:03 -0800

    bg app

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-      <Home />
+  <div class="container">
+    <Home />
+  </div>
 </template>
 
 <script>
@@ -20,5 +22,12 @@ export default {
 </script>
 
 <style>
-
+.container {
+  background: rgba(250, 250, 250, 0.068);
+  border-radius: 15px;
+  width: 305px;
+  height: 29.839rem;
+  box-shadow: 0px 8px 21px 3px rgba(117, 113, 113, 0.19);
+  border: 1px solid transparent;
+}
 </style>
